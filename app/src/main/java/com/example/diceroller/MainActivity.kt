2fad7cd8d0dd178dec76e9ba1840598b0673266f
Luciano Bracht <lucianoeletronico@gmail.com>
2023-01-25 19:41:23 -0300

    user can choose the number of sides of the dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 //This activity allows the user to roll a dice and view the result on the screen.
@@ -14,16 +16,43 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         // Create new Dice object with 6 sides
-        val dice6 = Dice(6)
+        //val dice6 = Dice(6)
+
 
         rollButton.setOnClickListener {
-            //val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            //toast.show()
+            val inputSides: EditText = findViewById(R.id.editTextNumberSides)
 
             val resultTextView: TextView = findViewById(R.id.textView)
+            /*  //tests to understand the error inputSides.text == "string"
+                //inputSides.text is printable, but it is not a string, it is an Editable type
+            println(inputSides.text.equals("(number of sides)"))
+            println(inputSides.text)
+            println("aa" == "aa")
+            println("aa".equals("aa"))
+            println(inputSides.text.toString() == "(number of sides)")
+            */
 
-            //Roll the dice and update the screen with the result
-            resultTextView.text = dice6.roll().toString()
+            if(inputSides.text.toString() == "") {
+                val dice2 = Dice(2)
+                resultTextView.text = dice2.roll().toString()
+                val toast = Toast.makeText(this, "Input not received, considering 2 side dice", Toast.LENGTH_SHORT)
+                toast.show()
+
+            } else if(inputSides.text.toString() == "0") {
+                val toast = Toast.makeText(
+                    this,
+                    "Please enter a number greater than zero",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+            } else {
+                val dicex = Dice(Integer.parseInt(inputSides.text.toString()))
+                //Roll the dice and update the screen with the result
+                resultTextView.text = dicex.roll().toString()
+            }
+
+
+
 
         }
     }
@@ -36,5 +65,21 @@ class Dice (private val sides: Int) {
 }
 
 //improvements for this app
+    //play more than one dice
+        //logic (backend)
+        //layout to show up to 50(?) results in the screen and the sum of them all
+    //let the user add some fix number (RPG purposes)
+        //create the field to receive this number
+        //logic (backend)
+    //let the user create presets (quantity of dices + fix number to sum with)
+        //layout
+        //logic (backend)
+    //pick up a random card
+        //layout
+        //logic
+    //layout to heads and tails
+    //menu to chose what user wants: (RPG preset, card, lottery preset)
+
+//done
     //choose how many faces someone wants the dice to have
     //if no value is entered, make it heads and tails and make a toast to say this is the default
